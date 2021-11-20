41d6ded9a2ec517c38ae428b2303719cd0dae674
Ephrem W <ephremworkeye@gmail.com>
2021-11-19 17:28:11 -0800

    bucket name changed to string

--- my_blog5/settings.py
+++ my_blog5/settings.py
@@ -160,7 +160,7 @@ django_heroku.settings(locals())
 
 AWS_S3_ACCESS_KEY_ID = os.environ.get('AWS_S3_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_STORAGE_BUCKET_NAME = str(os.environ.get('AWS_STORAGE_BUCKET_NAME'))
 
 
 AWS_S3_FILE_OVERWRITE = False
